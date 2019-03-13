98cc3cad483a83ba9fe0a49d2c384590417d4296
StayPure <48041104+StayPure@users.noreply.github.com>
2019-03-13 17:45:00 -0400

    Add files via upload

--- copy 2.c
+++ copy 2.c
@@ -8,67 +8,130 @@
 
 int main (int argc, char *argv[])
 {
-if (argc == 1) usage("copy");
-   else if (argc > 1) copyfiles(argc, argv);
+   void usage (char *progname);
+   void die (char *reason);
+   int copyfiles (int argc, char *argv[]);
+   int chkdst (/*int,*/ char **argv);
+
+   if (argc < 3)
+   {
+      void usage(char *progname);
+      char str[] = "copy", *strptr = str;
+      usage(strptr);
+   }
+   else if (!chkdst(&argv[argc - 1]))
+   {
+      void die (char *reason);
+      char str[] = "INVALID DESTINATION", *strptr = str;
+      die(strptr);
+   }
+
+   copyfiles(argc, argv);
+
+   exit(0);
 }
+
 
 void usage (char *progname)
 {
-
+   fprintf(stderr, "./%s file-path1 file-path2 ... dest-dir\n", progname); exit(1);
 }
+
 
 void die (char *reason)
 {
-
+   fprintf(stderr, "%s\nPROGRAM ENDED\n", reason); exit(1);
 }
 
-int chkdst (int argc, char **argv)
+
+int chkdst (/*int,*/ char **argv)
 {
+   int isdir (char *path);
 
+   if(isdir(*argv)) return 1;
+   fprintf(stderr, "INPUT DESTINATION DOES NOT EXIST\n");
+   return 0;
 }
 
-int isdir (char *path) 
+
+int isdir (char *path)
 {
-
+   struct stat sbuf;
+   if(stat(path, &sbuf) != 0) return 0;
+   return(S_ISDIR(sbuf.st_mode));
 }
+
 
 int isregular (char *path)
 {
-
+   struct stat sbuf;
+   stat(path, &sbuf);
+   return(S_ISREG(sbuf.st_mode));
 }
 
+
+int isvalid(char *path, char *dst)
+{
+   if (isdir(path))
+   {
+      fprintf(stderr, "%-5s: not a regular file\n", path);
+      return 0;
+   }
+   else if (!isregular(path))
+   {
+      fprintf(stderr, "%-5s: no such file or dir\n", path);
+      return 0;
+   }
+   else if (dst == NULL)
+   {
+      fprintf(stderr, "%-5s: dest creation error\n", path);
+      return 0;
+   }
+
+   return 1;
+}
+
+
 int copyfiles (int argc, char *argv[])
-{        
+{
    void buildpath (char *src, char *dst, char **dstpath);
    int makecp (char *srcpath, char *dstpath);
 
    int i = 1, copies = 0;
-   for (i = 1; i < argc; i++)
+   for (i = 1; i < argc - 1; i++)
    {
-      if (!isregular(argv[i]) || !isdir(argv[i])) continue;
-
       char *dstpath;
       buildpath(argv[i], argv[argc - 1], &dstpath);
+
+      if(!isvalid(argv[i], dstpath)) continue;
+
+      if (isregular(dstpath))
+      {
+         fprintf(stderr, "%-5s: exist at destination\n", argv[i]); continue;
+      }
 
       if (!makecp(argv[i], dstpath)) continue;
 
       copies++;
    }
 
-   if (!copies) return 1;
-
+   if (!copies) return 1;      
+   
+   fprintf(stderr, "\n%d copies created successfully.\n", copies);
    return 0;
 }
 
+
 int makecp (char *srcpath, char *dstpath)
 {
-   int fd, rd; /*char buffs[64];*/
+   int fd, rd, copy;  char bufs[64];
+   copy = open(dstpath, O_CREAT | O_WRONLY, 0644);
 
    if ((fd = open(srcpath, O_RDONLY)) < 0) return 0;
 
-   while((rd = read(fd, dstpath, rd)) > 0)
+   while((rd = read(fd, bufs, 64)) > 0)
    {
-      if(write(1, dstpath, rd) != rd)
+      if(write(copy, bufs, rd) != rd)
       {
          close(fd);
          return 0;
@@ -78,6 +141,7 @@ int makecp (char *srcpath, char *dstpath)
    close(fd);
    return 1;
 }
+
 
 void buildpath (char *src, char *dst, char **dstpath)
 {
@@ -101,6 +165,4 @@ void buildpath (char *src, char *dst, char **dstpath)
    {
       strcpy(*dstpath, dst); strcat(*dstpath, "/"); strcat(*dstpath, src);
    }
-
-   printf("%s \n", *dstpath);
 }
