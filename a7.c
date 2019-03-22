c5a19f8dc7629ff5d3bf1d084568f47858497286
StayPure <48041104+StayPure@users.noreply.github.com>
2019-03-21 21:34:39 -0400

    Update a7.c

--- a7.c
+++ a7.c
@@ -1,12 +1,52 @@
 /*
-	Name: Kevin C. Bell, Nicholas J. Bravata, Thomas J. Jury        Class: CPS 360
+        Name: Kevin C. Bell, Nicholas J. Bravata, Thomas J. Jury        Class: CPS 360
         Section: 22362460                                               Assignment: 07
-        Due: March 21, 2019                                          Started: March 19, 2019
+        Due: March 21, 2019                                             Started: March 19, 2019
         Credit: 10 points.
-        KEVIN CONT.
-*/        
+        
+        Problem: Write a progam that takes in a memory blocksize from the command-line that will
+        then be used to store random values of various sizes into the memory of the given blocksize
+        to fill it up and then use those values to get the sum of the random values. The program
+        needs to check the given blocksize to make sure it's a multple of 8, not less than or equal
+        to 0, or if the memory can be allocated for it. Afterwards, it stores byte (8-bit) size integers
+        in memory then finds the sum of those random integers, then it does the exact same thing for 
+        half-word (16-bit), word (32-bit), and double-word (64-bit), then printing it all out.
+
+        Solution: When the program starts it first makses sure that the currect amount of arguments
+        were give, if they weren't the program prints out the proper way to use it then ends, else
+        it goes through to check if the blocksize is a number and if it is if it's a muliple of 8, 
+        if both are true then it checks if its a positive number, if any of these don't check it the
+        program will bail out citing the specific reason. Now, the program will allocate memory to this
+        new block of memory (getmemblock()) that is the size of the give number this can not work giving
+        an allocation error and ending the program, after all of this is passed the program goes through
+        and fills the memory block with random byte sized (char) data (loadbytedata()) then adds these
+        new byte sized integers (sumbytedata()), The same is done with random 2 byte sized (short int)
+        data (loadhalfwdata() & sumhalfwdata()), random 4 byte sized (int) data (loadworddata() & 
+        sumworddata()), and random 8 byte sized (long long) data (loaddoublewdata() & sumdoublewdata()). 
+        After all of the sums have been found it is then printed to the stderr (printresult()), the 
+        memory for memory block is then released and the program ends with a EXIT_SUCCESS.
+
+        Data-structure used: A one-dimensional array for the command line arguments and for the memory
+        block.
+
+        Accessing functions for the data structure: Standard C functions for accessing arrays and 
+        malloc() to allocate the memory for the memory block array.
+
+        Errors handled: Checks the given arguemnt to make sure it is a number, its positive and 
+        that its a multiple of 8. The program also recoginzes when the memory is not able to allocate
+        memory. 
+
+        Limitations: The program does not work for any numbers at that are not multiples of 8 and at 
+        782 and above double-word sum overflows.
+
+        Acknowledgment: We wrote the program together but we had a lot of help from our classmates 
+        throughout the making of it.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+
 
 int main(int argc, char *argv[])
 {
@@ -42,12 +82,14 @@ int main(int argc, char *argv[])
    exit(0);   
 }
 
+
 /* THOMAS
 */
 void usage (char *progname)
 {
    fprintf(stderr, "./%s <blocksize>\n", progname); exit(1);
 }
+
 
 /* THOMAS
 */
@@ -58,11 +100,14 @@ char *getmemblock (int size)
    return str;
 }
 
-/* Design a module that takes in a char array and the size of that array then fills that array with
-   7-bit, size int values, that uses a random number generator.
-   Itterating through the array using a *(char *) to set the value using a random number generated in
-   the for loop. Then using the bit-wise operator '&' with 0x7F in order to change the first bit to a 0.
-   This continues until we go through the size of the array.
+
+/* Design a module that takes in a char array and the size of that 
+   array then fills that array with 7-bit, size int values, that 
+   uses a random number generator.
+   Itterating through the array using a *(char *) to set the value
+   using a random number generated in the for loop. Then using the 
+   bit-wise operator '&' with 0x7F in order to change the first bit
+   to a 0. This continues until we go through the size of the array.
 */
 void loadbytedata (char mem[], int size)
 {
@@ -71,14 +116,17 @@ void loadbytedata (char mem[], int size)
    {
       *((char *) & mem[i]) = rand() & 0x7F; 
    }
-   return;
 }
 
-/* Design a module that takes in a char array and the size of that array then fills that array with
-   15-bit, size/2 int values, that uses a random number generator.
-   Itterating through the array using a *(short in *) to set the value using a random number generated in
-   the for loop. Then using the bit-wise operator '&' with 0x7FFF in order to change the first bit to a 0.
-   This continues until we go through the size/2 of the array.
+
+/* Design a module that takes in a char array and the size of 
+   that array then fills that array with 15-bit, size/2 int values,
+   that uses a random number generator. 
+   Itterating through the array using a *(short in *) to set 
+   the value using a random number generated in the for loop. 
+   Then using the bit-wise operator '&' with 0x7FFF in order to 
+   change the first bit to a 0. This continues until we go through
+   the size/2 of the array.
 */
 void loadhalfwdata (char mem[], int size)
 {
@@ -86,11 +134,17 @@ void loadhalfwdata (char mem[], int size)
    for (i = 0; i < size; i += 2)
    {
       *((short int *) & mem[i]) = rand() & 0x7FFF;    
-   }
-   return; 
+   } 
 }
 
-/* KEVIN
+
+/* Design a module that fills up the memory array with
+   random 31-bit, size / 4 int values using random number
+   generator.
+   loops thorugh the memory array (mem[]) size times, with
+   each interation adding 4 to i to match the byte size of
+   word (int). the actual value of mem[i] is set to a random
+   value that has its sign bit removed with bitwise operations.
 */
 void loadworddata (char mem[], int size)
 {
@@ -99,10 +153,19 @@ void loadworddata (char mem[], int size)
    {    
       *((int *) & mem[i]) = rand() & 0x7FFFFFFF;   
    }
-   return; 
 }
 
-/* KEVIN
+/* Design a module that fills up the memory array with
+   random 63-bit, size / 8 int values using random number
+   generator.
+   loops through the memory array(mem[]) size times, with
+   each iteration adding 8 to i to match the byte size of
+   double-word (long long). The actual value of mem[i] is 
+   set to a random value but becaues it's a long long it 
+   needs two rand values to fill it out by filling out 
+   the first half with a random int and the rest with
+   another random int. This gives a complete random long
+   long that you then put into memory.
 */
 void loaddoublewdata (char mem[], int size)
 {
@@ -111,14 +174,15 @@ void loaddoublewdata (char mem[], int size)
    {
       *((long long *) & mem[i]) = ((long long) rand() << 31) | rand();   
    }
-   return;
 }
 
-/* Design a module that returns the sum of all the byte data in a char array.
-   Using a long long sum to store the sum of the char array. This module uses a 
-   for loop to itterate through the character array and uses a *(char*) to get the 
-   value of each byte and then adds it to the sum. Once the entire array has been 
-   processed the sum is returned.
+/* Design a module that returns the sum of all the byte 
+   data in a char array. Using a long long sum to store 
+   the sum of the char array. This module uses a for loop 
+   to itterate through the character array and uses a *(char*)
+   to get the value of each byte and then adds it to the 
+   sum. Once the entire array has been processed the sum 
+   is returned.
 */
 long long sumbytedata (char mem[], int size)
 {
@@ -131,11 +195,14 @@ long long sumbytedata (char mem[], int size)
    return sum;
 }
 
-/* Design a module that returns the sum of all the halfword data in a char array.
-   Using a long long sum to store the sum of the char array. This module uses a 
-   for loop to itterate through the character array and uses a *(short in*) to get the 
-   value of each halfword and then adds it to the sum. Once the entire array has been 
-   processed the sum is returned.
+/* Design a module that returns the sum of all the
+   halfword data in a char array.
+   Using a long long sum to store the sum of the char 
+   array. This module uses a for loop to itterate through
+   the character array and uses a *(short in*) to get the 
+   value of each halfword and then adds it to the sum. 
+   Once the entire array has been processed the sum is 
+   returned.
 */
 long long sumhalfwdata (char mem[], int size)
 {
@@ -148,7 +215,15 @@ long long sumhalfwdata (char mem[], int size)
    return sum;
 }
  
-/* KEVIN
+/* Design a module that finds the sum of the ints
+   (with no sign bits, 31-bit values) that are in 
+   the memory[].
+   Starts a sum outside of the loop, then loops 
+   through the memory (mem[]) adding only each int
+   value by setting the loop to add 4 to i every 
+   interation and casting the mem[i] to int. Returns
+   the sum of all ints in the
+   memory.
 */
 long long sumworddata (char mem[], int size)
 {
@@ -161,7 +236,15 @@ long long sumworddata (char mem[], int size)
    return sum;
 }
 
-/* KEVIN
+/* Design a module that finds the sum of the long
+   longs (with no sign bits, 63-bit values) that 
+   are in the memory[].
+   Starts a sum outside of the loop, then loops 
+   thorugh the memory (mem[]) adding only each
+   long long value by setting the loop to add 8
+   to i every interation and casting the mem[i]
+   to long long. Returns the sum of all
+   long longs in the memory.
 */
 long long sumdoublewdata(char mem[], int size)
 {
@@ -182,5 +265,4 @@ void printresult(long long bytesum, long long hwsum, long long wordsum, long lon
     fprintf(stderr, "%-20s : %-10lld\n", "Sum of half-words", hwsum);
     fprintf(stderr, "%-20s : %-10lld\n", "Sum of words", wordsum);
     fprintf(stderr, "%-20s : %-10lld\n\n","Sum of double-words", dwsum);
-    return;
 }
