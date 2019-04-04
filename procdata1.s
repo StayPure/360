979c0de533e70db5e309a76596659902d27e9cae
StayPure <48041104+StayPure@users.noreply.github.com>
2019-04-03 21:19:05 -0400

    Update procdata1.s

--- procdata1.s
+++ procdata1.s
@@ -1,3 +1,23 @@
+/* Design a module that takes in an array of test scores, number of 
+   students and then returns the lowest, highest, and mean of means 
+   using SPARC assembly.
+   Set L0 as the total sum of all the means, set L1 as the counter 
+   for the main loop, set L2 as the current mean, set L3 to the lowest
+   mean starting at 100 so its set to the first mean, set L4 to the 
+   highest mean, and set L5 to the number of students * 3 for easy
+   comparisons. The loop then starts by comparing i to nstudents * 3
+   if its greater than or equal to stop the loop otherwise continue
+   we add the ith students score in the array incrementing i for each
+   score and loading them so they can be added, then divide it by 3 to 
+   get that students mean. The mean is then add to the running sum and 
+   compared to the lowest mean and the highest mean to see if it is the 
+   lowest or highest thus far then i is incremented once more and the loop
+   is continued. When it is over the sum and number of students is divided
+   to get the mean of means lastly the lowest mean, highest mean, and mean 
+   of means are then stored in there respected addresses.
+*/
+
+
 .global procdata1
 procdata1: save %sp, -64, %sp
            clr %l0       
