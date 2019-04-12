c136b58701e6aa3db05cddc386b445144284eb67
StayPure <48041104+StayPure@users.noreply.github.com>
2019-04-12 13:36:52 -0400

    Add files via upload

---
+++ a9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+}
+
+
+/* KEVIN */
+void usage(char *progname)
+{
+
+}
+
+
+/* ANYONE */
+void die(char *reason)
+{
+
+}
+
+ 
+ /* THOMAS */
+int getfilesize(char *path)
+{
+
+}
+
+
+/* NICK */
+int loadimage(char *path, unsigned char mem[], int size)
+{
+
+}
+
+
+/* KEVIN */
+void mdump(unsigned char mem[], int size, int first, int last)
+{
+
+}
