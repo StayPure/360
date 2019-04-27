ae9f320385f61a7de7de3d9c5089d72056cb8cf8
StayPure <48041104+StayPure@users.noreply.github.com>
2019-04-27 17:47:35 -0400

    Complete Build, minimal comments

--- a10.c
+++ a10.c
@@ -1,52 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SETS 1024
 #define LINES 4
 struct cacheline{
-    char valid; /* for valid/invalid entry */
-    unsigned char age; /* for age of entry */
+    char valid; 
+    unsigned char age; 
     int tag;
 };
 typedef struct cacheline cache;
 cache l1[SETS * LINES];
-int reference, miss; /* keep track of cache misses */
+int reference, miss; 
+
 
 int main(int argc, char *argv[]) 
 {
     void checkargs(int argc, char* argv[]);
-    int isahit(unsigned ref);
     void initcache(void);
-    void printrslts();
-    void randomalgo(cache *line, unsigned ref);
-    void fifoalgo(cache *line, unsigned ref);
-    void processref(unsigned ref, cache *new);
-
+    void printrslts(char *policy);
+    void processreffile (int policy);
+    
     checkargs(argc, argv);
+    initcache();
+    srand(time(NULL));
     int victimpicker;
     if(strcmp(argv[1], "fifo")) victimpicker = 1;
     else  victimpicker = 0;
 
-    initcache();
+    processreffile(victimpicker);
 
-    int nextref;
-    while(scanf("%x", &nextref) != EOF)
-    {
-        reference++;
-        if (!isahit(nextref))
-        {
-            miss++;
-            cache new;
-            processref(nextref, &new);
-            
-            if (victimpicker) randomalgo(&new, nextref);
-            else fifoalgo(&new, nextref);
-        }
-    }
-    printrslts();
+    printrslts(victimpicker ? "Random" : "FIFO");
     exit(0);
 }
+
 
 void checkargs(int argc, char* argv[])
 {
@@ -54,9 +42,10 @@ void checkargs(int argc, char* argv[])
     void usage(char *progname);
 
     if (argc != 2) usage("a10");
-    else if (strcmp(argv[1],"fifo") && strcmp(argv[1],"random")) die("Polciy was incorrect, choices are: \"fifo\" or \"random\"");
-
+    else if (strcmp(argv[1],"fifo") && strcmp(argv[1],"random")) 
+        die("Polciy was incorrect, choices are: \"fifo\" or \"random\"");
 }
+
 
 /* Design a module that prints out the correct way to use the
    program using the progname to get the name of the program
@@ -70,21 +59,26 @@ void usage(char *progname)
     fprintf(stderr, "./%s <algo> < <inputfile>\n", progname); exit(1);
 }
 
+
 void die(char *reason)
 {
     fprintf(stderr, "%s\n", reason); exit(1);
 }
 
-int isahit(unsigned ref){
-    int indx = ((ref >> 6) & 0x3ff) << 2;
-    int tag = (ref >> 16) & 0xffff;
-    int i;
-
-    for(i=0; i<4; i++) if(l1[indx+i].valid && l1[indx+i].tag == tag) break;
-    
-    if(i == 4) return 0;
-    return 1;
+void printrslts(char *policy)
+{
+  fprintf(stdout, "%s:\t%d references, %d misses\n", policy, reference, miss);  
 }
+
+int isahit(unsigned ref){
+    int indx = ((ref >> 6) & 0x3ff) << 2
+        ,tag = (ref >> 16) & 0xffff
+        ,i;
+
+    for(i=0; i<4; i++) if(l1[indx+i].valid && l1[indx+i].tag == tag) return 1;
+    return 0;
+}
+
 
 void initcache(void)
 {   
@@ -97,10 +91,6 @@ void initcache(void)
     }
 }
 
-void printrslts()
-{
-  fprintf(stdout, "REFERNCES %d, MISSES: %d\n", reference, miss);  
-}
 
 void processref(unsigned ref, cache *new)
 {
@@ -111,31 +101,70 @@ void processref(unsigned ref, cache *new)
 }
 
 
+int emptyfiller (int indx, cache *line)
+{
+    int i;
+    for (i = indx; i < indx+4; i++) 
+    {
+        if (!l1[i].valid) 
+        {
+            l1[i] = *line;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
 void randomalgo(cache *line, unsigned ref)
 {
     int indx = ((ref >> 6) & 0x3ff) << 2;
 
-    int r = (rand() % (3 + 1));
     line->valid = 1;
+    if (emptyfiller(indx, line)) return;
+
+    int r = (rand() % (3 + 1));
     l1[indx + r] = *line;
 }
+
 
 void fifoalgo(cache *line, unsigned ref)
 {
     int indx = ((ref >> 6) & 0x3ff) << 2, i = 0;
     int max = 0, maxindx = 0;
-    for (i = indx; i < indx+4; i++)
+    line->valid = 1;
+    if (emptyfiller(indx, line)) return;
+
+    for (i = indx; i < (indx)+4; i++)
     {
         if (l1[i].age > max) 
         {
             max = l1[i].age; maxindx = i;
         }
     }
-    for (i = indx; i < indx+4; i++)
+
+    for (i = indx; i < (indx)+4; i++)
     {
         if (i == maxindx) continue;
         else l1[i].age++;
     }
-    line->valid = 1;
+
     l1[maxindx] = *line;
+}
+
+void processreffile (int policy)
+{
+    int nextref;
+    while(scanf("%x", &nextref) != EOF)
+    {
+        reference++;
+        if (!isahit(nextref))
+        {
+            miss++; cache new;
+            processref(nextref, &new);
+            
+            if (policy) randomalgo(&new, nextref);
+            else fifoalgo(&new, nextref);
+        }
+    }
 }
